24dad2d389f309e22131ce5ba86827dc672250c5
ling <ling@lings-MacBook-Air.local>
2023-11-08 15:14:41 -0600

    upload podcast_assistant

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# ai_assistant
+# AI Assistant
+
+Referencing OpenAI Assistant, I create my own ai assistant
+
+### Setup
+1. `pipenv shell`
+2. `pipenv install`
+3. need to update open api key in `.env.example` and change to `.env`
+
+### How to use
+
+- Podcast Assistant: This assistant reads transcript files of podcast and answers related questions about podcast
+
+
+### Reference
+[OpenAI Assistant](https://platform.openai.com/docs/assistants/overview)
---
+++ podcast_assistant.py
@@ -0,0 +1,93 @@
+from dotenv import load_dotenv 
+from openai import OpenAI
+import openai
+import os
+import time
+
+class CSVAssistant:
+    def __init__(self, data_folder):
+        self.client = OpenAI()
+        self.upload_files = []
+        self.upload(data_folder)
+        self.worker = self.create_assistant()
+    
+    def upload(self, folder_path):
+        for dirPath, dirNames, fileNames in os.walk(folder_path): 
+            for filename in fileNames:
+                if ".srt" in filename:
+                    file_path=os.path.join(dirPath, filename)
+                    file_obj = self.client.files.create(
+                        file=open(file_path, 'rb'),
+                        purpose='assistants',
+                        
+                    )
+                    self.upload_files.append(file_obj.id)
+        print(f"upload related file {self.upload_files}")
+
+    def create_assistant(self):
+        assistant = openai.beta.assistants.create(
+            instructions="You are helping to read transcript files of podcast and answering questions based on information from srt files",
+            name="MINEBOOK Podcast Assistant",
+            tools=[{"type": "retrieval"}],
+            model="gpt-4-1106-preview",
+            file_ids=self.upload_files,
+        )
+                            
+        return assistant
+    
+    def query(self, question):
+        print(f"query is {question}")
+        thread = openai.beta.threads.create()
+        thread_id = thread.id
+
+        message = self.client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content=question,
+        )
+
+        run = self.client.beta.threads.runs.create(
+            thread_id=thread_id,
+            assistant_id=self.worker.id)
+        
+        print(f"assistant starts running...")
+        start = time.time()
+        is_answer = False
+        while True:
+            run = self.client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run.id)
+            if run.completed_at:
+                elapsed = run.completed_at - run.created_at
+                elapsed = time.strftime("%H:%M:%S", time.gmtime(elapsed))
+                print(f"Run completed in {elapsed}")
+                is_answer = True
+                break
+            time.sleep(1)
+            current = time.time()
+            
+            if current-start > 30:
+                print(f"wait timeout! Can't get answer this time")
+                break
+        if is_answer:
+            messages = self.client.beta.threads.messages.list(thread_id=thread_id)
+            last_message = messages.data[0]
+
+            text = last_message.content[0].text.value
+            print(f"final answer: {text}")
+
+
+def main():
+    load_dotenv()
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    data_folder = os.path.join(project_dir, "data", "srt")
+    
+    assistant = CSVAssistant(data_folder)
+
+    while True:
+        question = input('輸入你的問題或是輸入quit離開: ')
+        if 'quit' in question:
+            break
+        assistant.query(question)
+        
+
+if __name__== "__main__":
+    main()
